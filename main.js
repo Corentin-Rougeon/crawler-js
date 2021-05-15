0e9c7d146b483d320adb3a003e4f94469f7a1a48
Morgan RIVIERE <morgan.riviere@ynov.com>
2021-05-15 17:54:19 +0200

    crawler ok, data ok, print file ok, select game ko

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-let games = []
 
 void (async () => {
     try {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.goto('https://www.goclecd.fr/acheter-cod-black-ops-cold-war-cle-cd-comparateur-prix/')
+        /*
+        await page.waitForSelector('div form div:nth-child(2) input');
+        await page.click('div form div:nth-child(2) input');
+        await page.keyboard.type(key_words);
+        await page.keyboard.press('Enter');
+         */
         const collectData = await page.evaluate(() => {
-            const grabData = (row, target) => row
-                .querySelector(`div.${target}`)
+
+            const grabData = (row, target) => row.querySelector(`.${target}`).innerText
 
             const DEALER_ROW = 'div.offers-table-row'
 
@@ -19,16 +24,19 @@ void (async () => {
 
             for (const mark of dealRow){
                 data.push({
-                    name: grabData(mark, 'offers-merchant'),
+                    name: grabData(mark, 'offers-merchant-name'),
                     price: grabData(mark, 'x-offer-price')
                 })
             }
             return data
         })
-        console.log(JSON.stringify(collectData, null, 2))
+
+        console.log(JSON.stringify(collectData,null, 2))
+
         await browser.close()
-        /*const jsonResult = JSON.stringify(collectData, null, 2)
-        await fs.promises.writeFile('result.json', jsonResult)*/
+        const jsonResult = JSON.stringify(collectData, null, 2)
+        await fs.promises.writeFile('result.json', jsonResult)
+
     } catch (error) {
         console.log(error)
     }
