bbbcc55951836962069368c97d76e6ff67daaeaf
Morgan RIVIERE <morgan.riviere@ynov.com>
2021-05-17 08:42:32 +0200

    navigation ko

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 
+//let game = prompt('Enter your game : ');
+let game = 'call of duty cold war'
+
 void (async () => {
     try {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-        await page.goto('https://www.goclecd.fr/acheter-cod-black-ops-cold-war-cle-cd-comparateur-prix/')
-        /*
-        await page.waitForSelector('div form div:nth-child(2) input');
-        await page.click('div form div:nth-child(2) input');
-        await page.keyboard.type(key_words);
-        await page.keyboard.press('Enter');
-         */
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.goclecd.fr/catalogue/search-call+of+duty+cold+war/');
+        /*await page.waitFor(1000);
+        await page.click('');
+        await page.waitFor(1000);
+        await page.type('input.banner-search-form-input', game);
+        await page.waitFor(1000);
+        await page.click('.banner-search-form-submit');
+        await page.waitFor(1000);*/
+        await page.click('.search-results-row-link');
+        await page.waitFor(1000);
+
         const collectData = await page.evaluate(() => {
-
             const grabData = (row, target) => row.querySelector(`.${target}`).innerText
-
             const DEALER_ROW = 'div.offers-table-row'
-
             const data = []
-
             const dealRow = document.querySelectorAll(DEALER_ROW)
-
             for (const mark of dealRow){
                 data.push({
                     name: grabData(mark, 'offers-merchant-name'),
